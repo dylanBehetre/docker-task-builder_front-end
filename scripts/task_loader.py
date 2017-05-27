8e27789204ea092cde29b1a9429d13ac30ba7c69
Antoine Parent <froow22@gmail.com>
2017-05-27 14:48:15 +0200

    Commit intermédiaire, POC pour savoir si un service s'est bien arrêté : TODO gérer s'il s'est mal arrêté x) task_loader bientôt terminé.

--- scripts/task_loader.py
+++ scripts/task_loader.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import json
+import subprocess
 
 # select * from priority where order_queue = (select min(order_queue) from priority);
 
@@ -23,15 +24,26 @@ if empty:
 else:
     table = "priority"
 
-commands = []
+commands = {}
 with sqlite3.connect('bdd/provisioning.db') as DBconn:
     c = DBconn.cursor()
     c.execute("SELECT command FROM "+ table +" WHERE order_queue = (SELECT min(order_queue) from "+table +");")
     data = c.fetchall()
-    for i in range(0, len(data)):
-        commands.append(data[i][0])
 DBconn.close()
-print(commands)
-print("from "+table)
+if data:
+    print("une des tables n'est pas vide")
+    for i in range(len(data)):
+        commands = {**commands, **eval(data[i][0])} #merge les dictionnaires
+    #on a récup les commandes à lancer
+    #on lance d'abord le split
+    subprocess.call(["docker","service","create","--mount","type=bind,src=/home/toinou/Documents,dst=/files","--mode=global","--restart-condition=none","--name=test","sjourdan/ffmpeg","-y","-i","/files/video.mp4","-acodec","copy","-f","segment","-vcodec","libx264","-reset_timestamps","1","-map","0","/files/output/d.mp4"])
+
+else:
+    print("les deux tables sont vides")
+
+
+
+
+
 ##simulation lancer service faire un bon nommage
 ##simulation savoir si script terminé dans docker service ps puis merge dans script différent
